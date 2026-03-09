1ba3a2f0a5e8c226f815fbc770e68d0079994b2f
Washington Costa <costasantana980@gmail.com>
2026-03-09 05:34:14 -0300

    Mudanças nas Validações

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-const botao = document.querySelector("#buscar")
-botao.addEventListener("click", buscarClima)
+const apiKey = "SUA_API_KEY";
 
-async function buscarClima(){
-   const cidade = document.querySelector("#cidade").value;
-   const chave = "dbc6a5cb187a5a7bda5addffc570667e";
-   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&units=metric&lang=pt_br`;
-   const resposta = await fetch(url);
-   const dados = await resposta.json();
+async function buscarClima() {
+  const cidade = document.querySelector("#cidade").value;
+  const container = document.querySelector("#resultado");
 
-    if(dados.cod !== 200){
-      alert("Erro: " + dados.message);
-      return;
+  if (cidade === "") {
+    container.innerHTML = "<p>Digite uma cidade.</p>";
+    return;
+  }
+
+  try {
+    const dados = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
+    );
+
+ 
+    if (!dados.ok) {
+      throw new Error("Cidade não encontrada");
     }
- 
-    mostrarClima(dados);
-}
 
-function mostrarClima(dados){
-  document.querySelector("#nomeCidade").textContent = dados.name;
+    const resposta = await dados.json();
+    
+    container.innerHTML = `
+      <div class="card-clima">
+        <h2>${resposta.name}</h2>
+        <p>🌡️ Temperatura: ${Math.round(resposta.main.temp)}°C</p>
+        <p>☁️ Clima: ${resposta.weather[0].description}</p>
+        <p>💧 Umidade: ${resposta.main.humidity}%</p>
+      </div>
+    `;
 
-  document.querySelector("#temperatura").textContent = "Temperatura: " + dados.main.temp + "°C"
-
-  document.querySelector("#descricao").textContent = "Clima: " + dados.weather[0].description
- 
-  document.querySelector("#umidade").textContent = "Umidade: " + dados.main.humidity + "%"
+  } catch (erro) {
+     container.innerHTML = `
+      <p style="color:white;">Erro: cidade não encontrada.</p>
+    `;
+  }
 }
